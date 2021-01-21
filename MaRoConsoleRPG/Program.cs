62498462206fb1e84dc5e57071c471c894343e55
Martin Roupé <19404726+MaRoupe@users.noreply.github.com>
2021-01-21 21:40:42 +0100

    Initial commit

---
+++ MaRoConsoleRPG/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MaRoConsoleRPG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi! Are you ready for an adventure? (Y/N)");
+            char playerInput = Console.ReadKey().KeyChar;
+            Console.Clear();
+            switch (Char.ToUpper(playerInput))
+            {
+                case 'Y':
+                    Console.WriteLine("Yay, Let's Go!");
+                    break;
+                case 'N':
+                    Console.WriteLine("Aww, why not?");
+                    break;
+                default:
+                    Console.WriteLine("Invalid input!");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
